5dd6413020a5230a92ae4210c259ca025ea5d2da
rabidima <dmitrirabinovich@Dmitris-MacBook-Pro.local>
2016-09-28 19:09:25 -0700

    redone reposotory

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends.
+// ===============================================================================
+
+
+var friendsArray = [
+	{
+		name: "Momo",
+		photo: "https://i.vimeocdn.com/portrait/7772033_300x300",
+		scores: [
+			2,
+			2,
+			2,
+			3,
+			4,
+			2,
+			3,
+			5,
+			1,
+			2
+		]
+	},
+	{
+		name: "Jecky",
+		photo: "http://www.american.edu/uploads/profiles/large/Jane_Hall_profile.jpg",
+		scores: [
+			1,
+			1,
+			2,
+			3,
+			4,
+			2,
+			3,
+			5,
+			1,
+			2
+		]
+	}
+];
+
+module.exports = friendsArray;
+
+			
